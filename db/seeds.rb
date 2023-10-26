239d8b29c7d93d78342c175fd829e36b0ca6ad18
brandonsnover <144728738+brandonsnover@users.noreply.github.com>
2023-10-26 12:15:04 -0500

    seeded initial data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# Song.create(title: "Master of Puppets", album: "Master of Puppets", artist: "Metallica", year: "1986")
+
+# Song.create(title: "Blinded by Fear", album: "Slaughter of the Soul", artist: "At the Gate", year: "1995")
+
+# Song.create(title: "My Own Grave", album: "Shaped bt Fire", artist: "As I Lay Dying", year: "2019")
+
+# Song.create(title: "Barbie Girl", album: "Aquarium", artist: "Aqua", year: "1997")
