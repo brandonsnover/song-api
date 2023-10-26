8cec5b3146c5072988d8fc9c04c48a7b11a752da
brandonsnover <144728738+brandonsnover@users.noreply.github.com>
2023-10-26 12:40:22 -0500

    added post/create route and controller

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,16 @@
+class SongsController < ApplicationController
+  def index
+    @songs = Song.all
+    render :index
+  end
+
+  def show
+    @song = Song.find_by(id: params[:id])
+    render :show
+  end
+
+  def create
+    @song = Song.create(title: params[:title], album: params[:album], artist: params[:artist], year: params[:year])
+    render :show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "/songs", controller: "songs", action: "index"
+  get "/songs/:id", controller: "songs", action: "show"
+  post "/songs", controller: "songs", action: "create"
 end
