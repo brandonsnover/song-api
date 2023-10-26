5a133d8b037d512b3c945f04575c9afe5f9258c5
brandonsnover <144728738+brandonsnover@users.noreply.github.com>
2023-10-26 12:48:18 -0500

    added update/post route and controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get "/songs", controller: "songs", action: "index"
   get "/songs/:id", controller: "songs", action: "show"
   post "/songs", controller: "songs", action: "create"
+  patch "/songs/:id", controller: "songs", action: "update"
 end
